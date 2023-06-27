d23eb75b7b75f5518ee76eebd65fc528f596ad97
AurelienNober <aurelien.nober@gmail.com>
2023-06-27 14:22:10 +0200

    fix webapp

--- webapps/named-entity-recognition-spacy/backend.py
+++ webapps/named-entity-recognition-spacy/backend.py
@@ -3,7 +3,7 @@ import json
 from flask import request
 from spacy import displacy
 
-from ner.spacy import get_spacy_model
+from ner.spacy import get_model
 
 
 @app.route("/run_NER")  # noqa
@@ -11,7 +11,7 @@ def run_NER():
     text = request.args.get("input", "")
     language = request.args.get("language", "en")
     print("Processing text '{}' in language '{}'...".format(text, language))
-    nlp = get_spacy_model(language=language)
+    nlp = get_model(model_id=language)
     doc = nlp(text)
     html = displacy.render(doc, style="ent", page=False)
     return json.dumps(html)
